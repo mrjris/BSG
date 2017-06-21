a8534de1506853e198e3021c7d50487a9e36630f
HoangTHb <HoangTHb@bkav.com>
2017-06-21 17:30:30 +0700

    Tách code ra cho nó tường minh.

--- BSG/Account.aspx.cs
+++ BSG/Account.aspx.cs
@@ -21,79 +21,76 @@ namespace BSG
         {
             
         }
-
+        private SqlConnection cnn = new SqlConnection(WebConfigurationManager.ConnectionStrings["BSGConnectionString"].ConnectionString);
         protected void Add_Click(object sender, EventArgs e)
         {
             if (!IsValid) return;
+
+            if (isAdd.Text == "true") Add_Account();
+            else Update_Account();
+            Add_Phone();
+        }
+
+        private void Add_Phone()
+        {
+            cnn.Open();
+            //Lấy ID của tài khoản vừa mới thêm
+            SqlCommand cmd = new SqlCommand("GetAccountID", cnn);
+            cmd.CommandType = CommandType.StoredProcedure;
+            int id = (int)cmd.ExecuteScalar();
+            cmd.Parameters.AddWithValue("@id", id);
+            //Xong lấy ID
             
+            //Thêm số điện thoại vào bảng Account_Phone (nếu có)
+            if (!validatePhones()) return;
+            cmd.CommandText = "AddPhone";
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.Parameters.Clear();
+            foreach (var sphone in phone)
+            {
+                cmd.Parameters.AddWithValue("@phone", sphone);
+                cmd.ExecuteNonQuery();
+                cmd.Parameters.Clear();
+            }
+            cmd.Dispose();
+            cnn.Close();
+        }
+
+        private void Update_Account()
+        {
+            cnn.Open();
+            SqlCommand cmd = new SqlCommand("GetAccountID", cnn);
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.Parameters.AddWithValue("@account", txtAccount.Text.Trim());
+            int accID = (int)cmd.ExecuteScalar();
+
+            cmd.CommandText = "UpdateAccount";
+            cmd.CommandType = CommandType.StoredProcedure;
             string pass = MD5.Encode(txtPassword.Text.Trim());
             string cp = Context.User.Identity.Name;
-            try
-            {
+            cmd.Parameters.AddWithValue("@id", accID);
+            cmd.Parameters.AddWithValue("@password", pass);
+            cmd.Parameters.AddWithValue("@description", txtDescription.Text.Trim());
+            cmd.Parameters.AddWithValue("@createdperson", cp);
+            cmd.ExecuteNonQuery();
+            cmd.Dispose();
+            cnn.Close();
+        }
 
-
-                SqlConnection cnn = new SqlConnection(WebConfigurationManager.ConnectionStrings["BSGConnectionString"].ConnectionString);
-                cnn.Open();
-                SqlCommand cmd;
-                //Thêm tài khoản mới vào bảng Account
-                if (isAdd.Text == "true")
-                {
-                    cmd = new SqlCommand("AddAccount", cnn);
-                    cmd.CommandType = CommandType.StoredProcedure;
-
-                    cmd.Parameters.Add("@account", txtAccount.Text.Trim());
-                    cmd.Parameters.Add("@password", pass);
-                    cmd.Parameters.Add("@description", txtDescription.Text.Trim());
-                    cmd.Parameters.Add("@createdperson", cp);
-                }
-                else
-                {
-                    cmd = new SqlCommand("UpdateAccount", cnn);
-                    cmd.CommandText = "GetAccountID";
-                    cmd.CommandType = CommandType.StoredProcedure;
-                    cmd.Parameters.Add("@account", txtAccount.Text.Trim());
-                    int accID = (int)cmd.ExecuteScalar();
-
-                    cmd.CommandText = "UpdateAccount";
-                    cmd.CommandType = CommandType.StoredProcedure;
-                    cmd.Parameters.Add("@id", accID);
-                    cmd.Parameters.Add("@password", pass);
-                    cmd.Parameters.Add("@description", txtDescription.Text.Trim());
-                    cmd.Parameters.Add("@createdperson", cp);
-                }
-                cmd.ExecuteNonQuery();
-                //Xong thêm tài khoản
-                dgvAccount.DataBind();
-
-                //Lấy ID của tài khoản vừa mới thêm
-                cmd.CommandText = "GetAccountID";
-                cmd.CommandType = CommandType.StoredProcedure;
-
-                int id = (int)cmd.ExecuteScalar();
-                cmd.Parameters.Add("@id", id);
-                //Xong lấy ID
-
-                
-                //Thêm số điện thoại vào bảng Account_Phone (nếu có)
-                if (!validatePhones()) return;
-                cmd.CommandText = "AddPhone";
-                cmd.CommandType = CommandType.StoredProcedure;
-                cmd.Parameters.Clear();
-                foreach (var sphone in phone)
-                {
-                    cmd.Parameters.AddWithValue("@phone", sphone);
-                    cmd.ExecuteNonQuery();
-                    cmd.Parameters.Clear();
-                }
-
-                cmd.Dispose();
-                cnn.Close();
-            }
-            catch (Exception ex)
-            {
-                isAdd.Text = ex.Message;
-            }
-
+        private void Add_Account()
+        {
+            cnn.Open();
+            SqlCommand cmd = new SqlCommand("AddAccount", cnn);
+            cmd.CommandType = CommandType.StoredProcedure;
+            string pass = MD5.Encode(txtPassword.Text.Trim());
+            string cp = Context.User.Identity.Name;
+            cmd.Parameters.AddWithValue("@account", txtAccount.Text.Trim());
+            cmd.Parameters.AddWithValue("@password", pass);
+            cmd.Parameters.AddWithValue("@description", txtDescription.Text.Trim());
+            cmd.Parameters.AddWithValue("@createdperson", cp);
+            cmd.ExecuteNonQuery();
+            cmd.Dispose();
+            cnn.Close();
         }
 
         private string[] phone;
