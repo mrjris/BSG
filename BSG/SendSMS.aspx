1d78936c61c67fe867fdf7a5e334f5abb957f975
HoangTHb <HoangTHb@bkav.com>
2017-06-23 16:44:20 +0700

    SendSMS

---
+++ BSG/SendSMS.aspx
@@ -0,0 +1,20 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="SendSMS.aspx.cs" Inherits="BSG.SendSMS" MasterPageFile="~/Site.Master" ValidateRequest="false" %>
+
+<asp:Content ContentPlaceHolderID="MainContent" runat="server">
+    <h1>Gửi tin nhắn</h1>
+    <hr />
+
+    <div class="row">
+        <div class="col-sm-4">Nội dung tin nhắn <span class="divStar"><a>*</a>:</span></div>
+        <div class="col-sm-8">
+            <asp:TextBox ID="txtMessage" placeholder="Cú pháp: <Mã nhóm> <Nội dung>" TextMode="MultiLine" Width="100%" runat="server" Rows="5" />
+            <asp:RequiredFieldValidator ErrorMessage="Tin nhắn không được để trống" ForeColor="Red" ControlToValidate="txtMessage" runat="server" /><br />
+            <asp:CustomValidator ID="ContentValidator" OnServerValidate="ContentValidator_ServerValidate" ForeColor="Red" ErrorMessage="Nội dung tin nhắn không được để trống" ControlToValidate="txtMessage" runat="server"></asp:CustomValidator><br />
+            <asp:CustomValidator ID="GroupCodeValidator" OnServerValidate="GroupCodeValidator_ServerValidate" ForeColor="Red" ErrorMessage="Mã nhóm không tồn tại. Bạn vui lòng kiểm tra lại." ControlToValidate="txtMessage" runat="server" /><br />
+            <asp:CustomValidator ID="AccountValidator" OnServerValidate="AccountValidator_ServerValidate" ErrorMessage="Bạn không có quyền gửi tin nhắn tới nhóm." ControlToValidate="txtMessage" ForeColor="Red" runat="server" ></asp:CustomValidator>
+        </div>
+    </div>
+    <div class="row">
+        <asp:Button ID="Send" OnClick="Send_Click" CssClass="pull-right btn btn-primary btn-default-bkav btn-bkav btn-bkav-right" Text="Gửi tin nhắn" runat="server" />
+    </div>
+</asp:Content>
---
+++ BSG/SendSMS.aspx.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Web.Configuration;
+namespace BSG
+{
+    public partial class SendSMS : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Send_Click(object sender, EventArgs e)
+        {
+
+        }
+        SqlConnection cnn = new SqlConnection(WebConfigurationManager.ConnectionStrings["BSGConnectionString"].ConnectionString);
+        protected void GroupCodeValidator_ServerValidate(object source, ServerValidateEventArgs args)
+        {
+            string[] dim = { "<", ">" };
+            string[] s = txtMessage.Text.Trim().Split(dim, StringSplitOptions.RemoveEmptyEntries);
+
+            if (s.Length < 2) args.IsValid = false;
+            else
+            {
+                cnn.Open();
+                SqlCommand cmd = cnn.CreateCommand();
+                cmd.CommandText = "CountByGroupCode";
+                cmd.CommandType = System.Data.CommandType.StoredProcedure;
+                cmd.Parameters.AddWithValue("@groupCode", s[0]);
+                int count = (int)cmd.ExecuteScalar();
+                if (count == 1) args.IsValid = true;
+                else args.IsValid = false;
+                cmd.Dispose();
+                cnn.Close();
+            }
+        }
+
+        protected void ContentValidator_ServerValidate(object source, ServerValidateEventArgs args)
+        {
+            string[] dim = { "<", ">" };
+            string[] s = txtMessage.Text.Trim().Split(dim, StringSplitOptions.RemoveEmptyEntries);
+
+            if (s.Length < 2) args.IsValid = false;
+            else args.IsValid = true;
+        }
+
+        protected void AccountValidator_ServerValidate(object source, ServerValidateEventArgs args)
+        {
+            string acc = User.Identity.Name;
+            args.IsValid = false;
+        }
+    }
+}
